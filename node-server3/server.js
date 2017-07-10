9f3f47f22d522d21e44f4f520c2cf4469fed8e92
gaozhidong <1011284295@qq.com>
2017-07-11 07:34:10 +0800

    使用函数传递让 HTTP 服务器工作

--- node-server3/server.js
+++ node-server3/server.js
@@ -1,9 +1,13 @@
 
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(3333);
+function onRequest(request,response){
+  response.writeHead(200,{"Content-Type":"text/plain"})
+  response.write("hello world")
+  response.send()
+}
+
+http.createServer(onRequest).listen(3333)
+
+
 console.log("localhost:3333")
