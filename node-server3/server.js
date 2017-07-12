970e337107514e6420ec096a21fe06beb0301ef5
gaozhidong <1011284295@qq.com>
2017-07-12 08:04:10 +0800

    以非阻塞操作进行请求响应

--- node-server3/server.js
+++ node-server3/server.js
@@ -1,19 +1,13 @@
 var http = require("http");
-var url = require("url")
+var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    response.writeHead(200, {
-      "Content-Type": "text/plain"
-    });
-    var content = route(handle, pathname)
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
   }
 
-  http.createServer(onRequest).listen(3333)
-
+  http.createServer(onRequest).listen(8888);
 }
 
 exports.start = start;
