b0990611205f73074b699ac334f662d7a4f73b78
gaozhidong <1011284295@qq.com>
2017-07-13 08:00:48 +0800

    我们不再需要传递postData了，这次要传递request对象

--- node-server3/router.js
+++ node-server3/router.js
@@ -1,9 +1,9 @@
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
   if (typeof handle[pathname] === 'function') {
-    handle[pathname](response, postData);
+    handle[pathname](response, request);
   } else {
     response.writeHead(404, {
-      "Content-Type": "text/plain"
+      "Content-Type": "text/html"
     });
     response.write("404 Not found");
     response.end();
